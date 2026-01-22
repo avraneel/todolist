1f0a03d810bdca74aa48b9eda904511c497840e1
Avraneel Pal <avraneelpal22@gmail.com>
2026-01-22 20:10:48 +0530

    making ProjectList

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@
 
 import { Project } from "./project.js"
 
-function createProject(name) {
-    return new Project("default");
-}
+// ProjectList stores the arrow function that returns an object,
+// thus making ProjectList an object
+const ProjectList = (() => {
 
-const proj1 = createProject("default");
+    const createProject = (name) => {
+        const proj = new Project(name);
+        return proj;
+    }
+
+    return {createProject};
+})();
+
+const proj1 = ProjectList.createProject("default");
 
 proj1.addTodo("Shop dinner", 0, "Buy food for dinner", "25th Jan, 2026");
 proj1.addTodo("Meditation", 1, "Mediate for 30 minutes", "3rd Feb, 2026");
 proj1.addTodo("Finish homework", 0, "Solve all 50 math problems", "2nd Jan, 2026");
+
+// localStorage.setItem(proj1.projectId, JSON.stringify(proj1));
 
 proj1.displayProject();
 
